8e2e1097931c45f416f4e7a356ccde92fea47640
logonrmlocal <logonrmlocal@L2201MICRO36>
2019-06-05 22:48:37 -0300

    Alteração Mensageria

--- SimpleBotCore/Controllers/MessagesController.cs
+++ SimpleBotCore/Controllers/MessagesController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Bot.Connector;
 using Microsoft.Bot.Schema;
 using SimpleBotCore.Logic;
+using System;
+using System.Threading.Tasks;
 
 namespace SimpleBotCore.Controllers
 {
--- SimpleBotCore/Logic/SimpleBotUser.cs
+++ SimpleBotCore/Logic/SimpleBotUser.cs
@@ -1,15 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 using System.Linq;
-using System.Web;
 
 namespace SimpleBotCore.Logic
 {
     public class SimpleBotUser
     {
+        private int contador = 0;
         public string Reply(SimpleMessage message)
         {
-            return $"{message.User} disse '{message.Text}'";
+            SaveMongoDB(message);
+
+            return $"Id: {message.Id} Usuario:{message.User} disse '{message.Text}'. Foram enviadas {GetUserByID(message)} mensagens por este usuario";
+        }
+
+        public void SaveMongoDB(SimpleMessage message)
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("15net");
+            var col = db.GetCollection<BsonDocument>("botcount");
+
+            var doc = new BsonDocument()
+            {
+                { "Id",message.Id },
+                { "Usuario",message.User},
+                { "Mensagem",message.Text}
+            };
+            
+           col.InsertOne(doc);
+
+            //contador++;
+        }
+
+        private int GetUserByID(SimpleMessage message)
+        {
+            int _userCount = 0;
+
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var db = client.GetDatabase("15net");
+            var col = db.GetCollection<BsonDocument>("botcount");
+
+            var filtro = Builders<BsonDocument>.Filter.Eq("Id", message.Id);
+            var res = col.Find(filtro).ToList();            if (res.Any())
+                _userCount = res.Count;            return _userCount;            
         }
 
     }
